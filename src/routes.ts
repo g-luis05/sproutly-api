ed3305a40575cfa1e7e3018677770cb02a6f8d6f
Luis <hdluish@gmail.com>
2026-02-01 17:15:02 -0500

    Router in routes.ts

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+
+export class AppRoutes {
+
+    static get routes() {
+
+        const router = Router();
+
+        // router.use('/api/v1/auth');
+        // router.use('/api/v1/topics');
+        // router.use('/api/v1/decisions');
+        // router.use('/api/v1/users');
+
+        return router;
+
+    }
+
+}
