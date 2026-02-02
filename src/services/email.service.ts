1809db48c742bd906614b89e0244f60d4e9cb0b5
Luis <hdluish@gmail.com>
2026-02-02 17:11:03 -0500

    Demo sendEmail

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -5,7 +5,7 @@ export class EmailService {
     //TODO
 
     //Demo
-    sendEmail(email: string, code: string) {
+    static async sendEmail(email: string, code: string) {
         console.log(`Email sent to ${ email } with code ${ code }`);
     }
 
