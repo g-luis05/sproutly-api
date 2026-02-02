88339f9a71e94a104706b3302038f0c391446804
Luis <hdluish@gmail.com>
2026-02-02 16:09:00 -0500

    Email service demo

---
+++ src/services/email.service.ts
@@ -0,0 +1,12 @@
+
+
+export class EmailService {
+
+    //TODO
+
+    //Demo
+    sendEmail(email: string, code: string) {
+        console.log(`Email sent to ${ email } with code ${ code }`);
+    }
+
+}
