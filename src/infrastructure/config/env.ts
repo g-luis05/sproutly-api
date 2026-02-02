500f1f68bcf2b2803d131d9d1faac6abb35d48f7
Luis <hdluish@gmail.com>
2026-02-02 14:32:03 -0500

    JWT secret added to env-var adapter

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -4,5 +4,9 @@ import { get } from 'env-var';
 export const envs = {
 
     PORT: get('PORT').required().asPortNumber(),
+    
+
+    // JWT
+    JWT_SECRET: get('JWT_SECRET').required().asString(),
 
 }
---
+++ src/infrastructure/config/jwt.adapter.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+import { envs } from './env';
+
+
+const jwtSecret = envs.JWT_SECRET;
+
+export class JwtAdapter {
+
+    //TODO generate Token and verify token
+
+}
+
