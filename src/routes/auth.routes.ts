0e9ba7865d48e85a1b8139ffd8f2b80a41c17042
Luis <hdluish@gmail.com>
2026-02-03 16:11:24 -0500

    Validation middleware path added before using the auth controller

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "../controllers";
+import { requestOtpSchema, verifyOtpSchema } from "../validators";
+import { ValidateMiddleware } from "../middlewares";
 
 
 export class AuthRoutes {
@@ -7,7 +9,18 @@ export class AuthRoutes {
     static get routes(): Router {
 
         const router = Router();
-        router.post( '/request-otp', AuthController.requestOtp);
+
+        router.post( '/request-otp',
+            ValidateMiddleware.validateBody(requestOtpSchema),
+            AuthController.requestOtp,
+        );
+
+        router.post( '/verify-otp', 
+            ValidateMiddleware.validateBody(verifyOtpSchema), 
+            AuthController.verifyOtp,
+        );
+
+        
         return router;
 
     }
