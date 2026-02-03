8ba4fcd09bd37f61696aaed0b01ffa61b2e39dcc
Luis <hdluish@gmail.com>
2026-02-03 16:12:40 -0500

    Some comments

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
+import { CustomError } from "../domain/errors/custom.error";
 import { hashAdapter } from "../infrastructure/config/bcrypt.adapter";
-import { OtpRepository } from "../repositories/otp.repository";
-import { UserRepository } from "../repositories/user.repository";
+import { jwtAdapter } from "../infrastructure/config/jwt.adapter";
+import { OtpRepository, UserRepository } from "../repositories";
 import { EmailService } from "./email.service";
 
 
@@ -12,7 +13,10 @@ export class AuthService {
         if (!user) {
             user = await UserRepository.create(email);
         }
+        // Invalidate any active OTP codes
+        await OtpRepository.invalidateActive(user.id);
 
+        //annd generate here
         const code = Math.floor(100000 + Math.random() * 900000).toString();
         
         const codeHash = await hashAdapter.hash(code);
@@ -24,7 +28,45 @@ export class AuthService {
             expiresAt
         });
 
-        await EmailService.sendEmail(email, codeHash);
+        await EmailService.sendEmail(email, code);
+
+    }
+
+    static async verifyOtp( email: string, code: string ) {
+
+        if (!email || !code) {
+            throw CustomError.badRequest('Email and code are required');
+        }
+
+        const user = await UserRepository.findByEmail(email);
+        if (!user) throw CustomError.badRequest('User not found');
+        
+
+        const otp = await OtpRepository.findActive(user.id);
+        if (!otp) throw CustomError.badRequest('OTP not found');
+
+        if (otp.attempts >= 3) {
+            await OtpRepository.markUsed(otp.id);
+            throw CustomError.forbidden('Too many attempts');
+        }
+
+        const isValid = await hashAdapter.compare(code, otp.codeHash);
+        if (!isValid) {
+            await OtpRepository.increaseAttempts(otp.id);
+            throw CustomError.unauthorized('Invalid code');
+        }
+
+        await OtpRepository.markUsed(otp.id);
+        await UserRepository.updateLastLogin(user.id);
+
+        const token = jwtAdapter.generateToken({
+            id: user.id,
+            email: user.email
+        });
+
+        console.log( token); //Development
+
+        return { token };
 
     }
 
