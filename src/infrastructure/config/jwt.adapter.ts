8f26a961ae41de448c52154ff3d7b26caff6c102
Luis <hdluish@gmail.com>
2026-02-03 16:07:29 -0500

    Jwt adapter done

--- src/infrastructure/config/jwt.adapter.ts
+++ src/infrastructure/config/jwt.adapter.ts
@@ -2,7 +2,21 @@ import jwt from 'jsonwebtoken';
 import { envs } from './env';
 
 
-const jwtSecret = envs.JWT_SECRET;
+const JWT_SEED = envs.JWT_SECRET;
 
-export const jwtAdapter = {};
+export const jwtAdapter = {
+    generateToken(payload: any, duration: number = 60 * 15) {
+        return jwt.sign(payload, JWT_SEED, { expiresIn: duration });
+    },
+    verifyToken<T>(token: string): Promise<T | null> {
+
+        return new Promise((resolve) => {
+            jwt.verify(token, JWT_SEED, (err, decoded) => {
+                if (err) return resolve(null);
+                resolve(decoded as T);
+            });
+        })
+    },
+
+};
 
