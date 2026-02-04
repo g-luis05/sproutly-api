0cd0b2e141082a01e92c1e4aaca140ecd2ff9d42
Luis <hdluish@gmail.com>
2026-02-03 22:28:38 -0500

    topics routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { HealthRoutes } from "./routes/health.routes";
 import { ProtectedRoutes } from "./routes/protected.routes";
 import { AuthRoutes } from "./routes/auth.routes";
+import { TopicRoutes } from "./routes/topic.routes";
 
 
 export class AppRoutes {
@@ -11,7 +12,7 @@ export class AppRoutes {
         const router = Router();
 
         router.use('/api/v1/auth', AuthRoutes.routes);
-        // router.use('/api/v1/topics');
+        router.use('/api/v1/topics', TopicRoutes.routes);
         // router.use('/api/v1/decisions');
         // router.use('/api/v1/users');
         router.use('/api/v1/health', HealthRoutes.routes);
