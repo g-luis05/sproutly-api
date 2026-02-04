ef6c96c2d15aa4d8d5a935b614357ab997fd89af
Luis <hdluish@gmail.com>
2026-02-03 22:33:56 -0500

    CRUD topic service

---
+++ src/services/topic.service.ts
@@ -0,0 +1,39 @@
+import { CustomError } from "../domain/errors/custom.error";
+import { CreateTopicDTO, DeleteTopicDTO, UpdateTopicDTO } from "../dtos";
+import { TopicRepository } from "../repositories/topic.repository";
+
+
+
+export class TopicService {
+
+    static async createTopic(dto: CreateTopicDTO) {
+        await TopicRepository.create(dto);
+    }
+
+    static async findAllTopics(userId: string) {
+        const topics = await TopicRepository.findAll(userId);
+        return topics;
+    }
+
+    static async updateTopic(dto: UpdateTopicDTO) {
+        const data: { title?: string; description?: string; } = {};
+
+        if ( dto.title !== undefined ) data.title = dto.title;
+        if ( dto.description !== undefined ) data.description = dto.description;
+
+        if ( Object.keys(data).length === 0 ) return true;
+
+        const updated = await TopicRepository.updateById(dto.id, dto.userId, data);
+
+        if ( updated.count === 0 ) throw CustomError.notFound('Topic not found');
+
+        return true;
+    }
+
+    static async deleteTopic(dto: DeleteTopicDTO) {
+        const deleted = await TopicRepository.deleteById(dto.id, dto.userId);
+        if ( deleted.count === 0 ) throw CustomError.notFound('Topic not found');
+        return true; 
+    }
+
+}
