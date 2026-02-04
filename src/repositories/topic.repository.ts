6a861b4201007826cad2d2c0e15198caaf41b4ae
Luis <hdluish@gmail.com>
2026-02-03 22:31:41 -0500

    CRUD topic repository

---
+++ src/repositories/topic.repository.ts
@@ -0,0 +1,29 @@
+import prisma from "../infrastructure/lib/prisma";
+
+
+export const TopicRepository = {
+
+    create(data: {
+        title: string;
+        description?: string;
+        userId: string;
+    }) {
+        return prisma.topic.create({ data });
+    },
+    findAll(userId: string) {
+        return prisma.topic.findMany({ where: { userId: userId }, orderBy: { createdAt: "desc" } });
+    },
+    updateById(id: string, userId: string, data: { title?: string; description?: string; }) {
+        return prisma.topic.updateMany({
+            where: { id, userId },
+            data,
+        });
+    },
+    deleteById(id: string, userId: string) {
+        return prisma.topic.deleteMany({
+            where: { id, userId},
+        });
+    },
+    
+
+}
