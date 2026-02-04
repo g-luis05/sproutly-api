c75d7466217e5f4750c165aefc23337125d2e845
Luis <hdluish@gmail.com>
2026-02-03 22:32:17 -0500

    Adding topic routes with the auth token, validator bodies and validator params

---
+++ src/routes/topic.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { AuthMiddleware, ValidateMiddleware } from "../middlewares";
+import { createTopicSchema, deleteTopicSchemaParams, updateTopicParamsSchema, updateTopicSchema } from "../validators/topic.validator";
+import { TopicController } from "../controllers/topic.controller";
+
+
+export class TopicRoutes {
+
+    static get routes(): Router {
+
+        const router = Router();
+
+        router.post( '/',
+            AuthMiddleware.verifyToken,
+            ValidateMiddleware.validateBody( createTopicSchema ),
+            TopicController.createTopic,
+        );
+
+        router.get('/',
+            AuthMiddleware.verifyToken,
+            TopicController.findAllTopics,
+        );
+
+        router.patch('/:id',
+            AuthMiddleware.verifyToken,
+            ValidateMiddleware.validateParams( updateTopicParamsSchema ),
+            ValidateMiddleware.validateBody( updateTopicSchema ),
+            TopicController.updateTopic,
+        );
+
+        router.delete('/:id',
+            AuthMiddleware.verifyToken,
+            ValidateMiddleware.validateParams( deleteTopicSchemaParams ),
+            TopicController.deleteTopic,
+        );
+        
+        return router;
+
+    }
+
+}
