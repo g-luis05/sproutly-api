697a3e424ecfbc9e4cf2f87cc8134359ad05da9b
Luis <hdluish@gmail.com>
2026-02-03 22:27:27 -0500

    Verify token method to use in protected routes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,8 +1,33 @@
 import { Request, Response, NextFunction } from "express";
+import { jwtAdapter } from "../infrastructure/config/jwt.adapter";
 
 export class AuthMiddleware {
 
     //TODO: auth middleware
+
+    static async verifyToken( req: Request, res: Response, next: NextFunction ) {
+    
+        const authHeader = req.headers.authorization;
+        if (!authHeader?.startsWith('Bearer ')) return res.status(401).json({ message: 'Missing auth header' });
+
+        const token = authHeader.split(' ')[1];
+        if (!token) return res.status(401).json({ message: 'Invalid auth format' });
+
+        const payload = await jwtAdapter.verifyToken<{
+            id: string;
+            email: string;
+        }>(token);
+
+        if (!payload) return res.status(401).json({ message: 'Invalid or expired token' });
+
+        // From global type, express augmentation req.user
+        req.user = payload;
+
+        //TODO - Refresh token
+
+        return next();
+    }
+
 
     // Demo
 
