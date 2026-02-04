bfcf05d78d5a7173e60f757f75e3b3b22dfd060e
Luis <hdluish@gmail.com>
2026-02-04 18:51:05 -0500

    Finished decision routes

---
+++ src/routes/decision.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { AuthMiddleware, ValidateMiddleware } from "../middlewares";
+import { createDecisionSchema, idValidatorSchemaParams, updateDecisionSchema } from "../validators";
+import { DecisionController } from "../controllers/decision.controller";
+
+
+export class DecisionRoutes {
+
+    static get routes(): Router {
+
+        const router = Router();
+
+        router.post('/',
+            AuthMiddleware.verifyToken,
+            ValidateMiddleware.validateBody(createDecisionSchema),
+            DecisionController.createDecision,
+        );
+
+        router.get('/:id/children',
+            AuthMiddleware.verifyToken,
+            ValidateMiddleware.validateParams(idValidatorSchemaParams),
+            DecisionController.findChildren,
+        );
+
+        router.patch('/:id',
+            AuthMiddleware.verifyToken,
+            ValidateMiddleware.validateParams(idValidatorSchemaParams),
+            ValidateMiddleware.validateBody(updateDecisionSchema),
+            DecisionController.updateDecision,
+        );
+
+        router.delete('/:id',
+            AuthMiddleware.verifyToken,
+            ValidateMiddleware.validateParams(idValidatorSchemaParams),
+            DecisionController.deleteDecision,
+        ); 
+
+        return router;
+    }
+
+}
