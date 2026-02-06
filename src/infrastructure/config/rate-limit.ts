5afb4a8a87751a7eb3b840cd6d4d7f77380e647b
Luis <hdluish@gmail.com>
2026-02-06 15:53:23 -0500

    request otp limiter, verify otp limiter and refresh limiter

---
+++ src/infrastructure/config/rate-limit.ts
@@ -0,0 +1,30 @@
+import rateLimit from "express-rate-limit";
+
+
+export const requestOtpLimiter = rateLimit({
+
+    windowMs: 60 * 1000,
+    max: 5,
+    message: "Too many requests from this IP, please try again after 1 minute",
+    standardHeaders: true,
+    legacyHeaders: false,
+    
+});
+
+export const verifyOtpLimiter = rateLimit({
+    windowMs: 15*60*1000,
+    max: 10,
+    message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+export const refreshLimiter = rateLimit({
+    windowMs: 15*60*1000,
+    max: 30,
+    message: "Too many renewal requests from this IP, please try again after 15 minutes",
+});
+
+export const apiLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 20,
+    message: "Too many creations. Please try again after 1 minute",
+});
