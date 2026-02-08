9c30a89fa593e92cb34d41ef224b72bc451db85a
Luis <hdluish@gmail.com>
2026-02-08 15:50:39 -0500

    email service migrate from nodemailer to resend

--- src/infrastructure/email/email.service.ts
+++ src/infrastructure/email/email.service.ts
@@ -1,3 +1,4 @@
+import { Resend } from "resend";
 import nodemailer from "nodemailer";
 import { envs } from "../config/env";
 import { otpEmailTemplate } from "./otp-email";
@@ -5,26 +6,49 @@ import { otpEmailTemplate } from "./otp-email";
 
 export class EmailService {
 
-    private static transporter = nodemailer.createTransport({
-        host: envs.EMAIL_HOST,
-        port: envs.EMAIL_PORT,
-        auth: {
-            user: envs.EMAIL_USER,
-            pass: envs.EMAIL_KEY,
-        },
-    });
+    // private static transporter = nodemailer.createTransport({
+    //     host: envs.EMAIL_HOST,
+    //     port: envs.EMAIL_PORT,
+    //     auth: {
+    //         user: envs.EMAIL_USER,
+    //         pass: envs.EMAIL_KEY,
+    //     },
+    // });
 
-    static async sendOtp( email: string, code: string ) {
+   private static resend = new Resend(envs.RESEND_API_KEY);
 
+   static async sendOtp(email: string, code: string) {
+    try {
         const htmlBody = otpEmailTemplate(code);
 
-        await this.transporter.sendMail({
-            from: `SproutlyApp - ${ envs.EMAIL_USER }`,
+        const { data, error } = await this.resend.emails.send({
+            from: envs.EMAIL_FROM,
             to: email,
-            subject: "SproutlyApp verification code",
+            subject: 'Sproutly App verification code',
             html: htmlBody,
-
         });
+
+        if (error) {
+            console.error('Error sending email:', error);
+        }
+    } catch (error) {
+        console.error('Error sending email:', error);
     }
+
+   }
+
+//    static async sendOtpWithSMTP(email: string, code: string) {
+
+//     const htmlBody = otpEmailTemplate(code);
+
+//         await this.transporter.sendMail({
+//             from: `SproutlyApp - ${ envs.EMAIL_USER }`,
+//             to: email,
+//             subject: "SproutlyApp verification code",
+//             html: htmlBody,
+
+//         });
+
+//    }
 
 }
