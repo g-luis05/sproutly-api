a2b76561b339ab403400d8171204ad9632db57ab
Luis <hdluish@gmail.com>
2026-02-08 16:06:00 -0500

    Deleting envs not used in production

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -10,10 +10,10 @@ export const envs = {
     JWT_SECRET: get('JWT_SECRET').required().asString(),
 
     // Email - dev
-    EMAIL_HOST: get('EMAIL_HOST').required().asString(),
-    EMAIL_PORT: get('EMAIL_PORT').required().asPortNumber(),
-    EMAIL_USER: get('EMAIL_USER').required().asString(),
-    EMAIL_KEY: get('EMAIL_KEY').required().asString(),
+    // EMAIL_HOST: get('EMAIL_HOST').required().asString(),
+    // EMAIL_PORT: get('EMAIL_PORT').required().asPortNumber(),
+    // EMAIL_USER: get('EMAIL_USER').required().asString(),
+    // EMAIL_KEY: get('EMAIL_KEY').required().asString(),
 
     //Email - Production
     RESEND_API_KEY: get('RESEND_API_KEY').required().asString(),
