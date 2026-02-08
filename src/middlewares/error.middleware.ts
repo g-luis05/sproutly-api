ec8020564c88635c6837450bffd1e84f4e90e639
Luis <hdluish@gmail.com>
2026-02-08 15:06:35 -0500

    Debugging deploy error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,8 +4,15 @@ import { CustomError } from "../domain/errors/custom.error";
 export class ErrorMiddleware {
 
     static handleError(
-        error: unknown, req: Request, res: Response, next: NextFunction,
+        error: any, req: Request, res: Response, next: NextFunction,
     ) {
+        console.error('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
+        console.error('❌ ERROR CAUGHT BY MIDDLEWARE:');
+        console.error('Route:', req.method, req.path);
+        console.error('Error message:', error.message);
+        console.error('Error stack:', error.stack);
+        console.error('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
+
         if (error instanceof CustomError) {
             return res.status(error.statusCode).json( { message: error.message } );
         } else {
