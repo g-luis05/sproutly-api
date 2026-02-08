07d5079518be2b49031485ce27fe8c0e7135193e
Luis <hdluish@gmail.com>
2026-02-08 15:43:16 -0500

    Implemented new env variables, resend api key and email from

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -9,10 +9,14 @@ export const envs = {
     // JWT
     JWT_SECRET: get('JWT_SECRET').required().asString(),
 
-    //Email
+    // Email - dev
     EMAIL_HOST: get('EMAIL_HOST').required().asString(),
     EMAIL_PORT: get('EMAIL_PORT').required().asPortNumber(),
     EMAIL_USER: get('EMAIL_USER').required().asString(),
     EMAIL_KEY: get('EMAIL_KEY').required().asString(),
+
+    //Email - Production
+    RESEND_API_KEY: get('RESEND_API_KEY').required().asString(),
+    EMAIL_FROM: get('EMAIL_FROM').required().asString(),
 
 }
